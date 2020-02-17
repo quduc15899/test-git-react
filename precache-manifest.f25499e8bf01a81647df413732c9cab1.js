6cf4a2afd01f91dca55cbdb7608dd9c4135039e0
= <=>
2020-02-17 15:40:33 +0700

    Updates

---
+++ precache-manifest.f25499e8bf01a81647df413732c9cab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbab930bc406019f0b20cc8eff6ba705",
+    "url": "/quduc15899/test-git-react.git/index.html"
+  },
+  {
+    "revision": "5e8813509a312097d43d",
+    "url": "/quduc15899/test-git-react.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9296a11ca5656f841faa",
+    "url": "/quduc15899/test-git-react.git/static/js/2.aecb8221.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quduc15899/test-git-react.git/static/js/2.aecb8221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e8813509a312097d43d",
+    "url": "/quduc15899/test-git-react.git/static/js/main.72b9c654.chunk.js"
+  },
+  {
+    "revision": "c5933624c3537811fd01",
+    "url": "/quduc15899/test-git-react.git/static/js/runtime-main.9cbc7b6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quduc15899/test-git-react.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
